7c067a18db3c6f39d0a81dfa070a7748adedf1f6
Cthulhu <joly534@gmail.com>
2022-11-25 10:42:10 +0100

    update

--- js/teleport.js
+++ js/teleport.js
@@ -4,10 +4,20 @@ let targetZ;
 let posTargetX;
 let posTargetZ;
 
-let camera = document.querySelector('#cam-rig');
 let ease = 0.05;
 let valX = 0;
 let valZ = 0;
+
+// function getPosCamera() {
+//     setInterval(() => {
+//         let posCamera = camera.getAttribute('position');
+//         let rotCamera = camera.getAttribute('rotation');
+//         console.log(posCamera, rotCamera);
+        
+//     }, 1000);
+// }
+// getPosCamera()
+
 
 AFRAME.registerComponent('teleport', {
 
@@ -27,14 +37,37 @@ AFRAME.registerComponent('teleport', {
             })          
               
             points[i].addEventListener('click', function() {
-                posTargetX = points[i].object3D.position.x;
-                posTargetZ = points[i].object3D.position.z;
-                posTargetY = points[i].object3D.position.y;
-                console.log(posTargetX, posTargetZ);
+                posX = points[i].object3D.position.x;
+                posZ = points[i].object3D.position.z;
+                posY = points[i].object3D.position.y;
+
+                posParentX = points[i].object3D.parent.position.x;
+                posParentZ = points[i].object3D.parent.position.z;
+                posParentY = points[i].object3D.parent.position.y;
+
+                posGrandParentX = points[i].object3D.parent.parent.position.x;
+                posGrandParentZ = points[i].object3D.parent.parent.position.z;
+                posGrandParentY = points[i].object3D.parent.parent.position.y;
+
+                posTargetX = posGrandParentX - posParentX - posX
+                posTargetZ = posGrandParentY - posParentY - posY
+                posTargetY = posGrandParentZ - posParentZ - posZ
+
+                rotX = points[i].object3D.rotation.x;
+                rotZ = points[i].object3D.rotation.z;
+                rotY = points[i].object3D.rotation.y;
+
+                console.log(posX, posParentX, posGrandParentX);
+                console.log(points[i].object3D.parent.rotation.x,points[i].object3D.parent.parent.rotation.y);
                 camera.setAttribute('position', {
                     x:posTargetX,
                     y:posTargetY,
                     z:posTargetZ
+                })
+                camera.setAttribute('rotation', {
+                    x:rotTargetX,
+                    y:rotTargetZ,
+                    z:rotTargetY
                 })
 
             })
