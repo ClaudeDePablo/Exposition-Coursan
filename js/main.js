278c230ce3025878402b2a37137a2b81a6ece966
Cthulhu <joly534@gmail.com>
2022-12-06 15:40:55 +0100

    update
    regle la lumiere, enleve les stats et les déplacements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // ecoute des controllers VR
 
 let sc = document.querySelector('a-scene');
-let camera = document.querySelector('#cam-rig');
 
 let leftController = document.getElementById('left-hand');
 
